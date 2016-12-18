a1bef85750d069f43235c7fb0b5abf8b76368d87
Kyle Polley <kpolley@outlook.com>
2016-12-17 21:30:02 -0500

    created experience card transition

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,48 @@
 $(window).resize(function(){
 
 	$('#summary').css({
-		position:'fixed',
+
 		left: ($(window).width() - $('#summary').outerWidth())/2,
 		top: ($(window).height() - $('#summary').outerHeight())/2
 	});
 
 	$('#education').css({
-		position:'fixed',
+
 		left: ($(window).width() - $('#education').outerWidth())/2,
 		top: ($(window).height() - $('#education').outerHeight())/2
 	});
 
 	$('#experience').css({
-		position:'fixed',
+
 		left: ($(window).width() - $('#experience').outerWidth())/2,
 		top: ($(window).height() - $('#experience').outerHeight())/2
 	});
 
+	$('#experience1').css({
+
+		left: ($(window).width() - $('#experience1').outerWidth())/2,
+		top: ($(window).height() - $('#experience1').outerHeight())/2
+	});
+
 	$('#projects').css({
-		position:'fixed',
+
 		left: ($(window).width() - $('#projects').outerWidth())/2,
 		top: ($(window).height() - $('#projects').outerHeight())/2
 	});
 
 	$('#knowledge').css({
-		position:'fixed',
+
 		left: ($(window).width() - $('#knowledge').outerWidth())/2,
 		top: ($(window).height() - $('#knowledge').outerHeight())/2
 	});
 
 	$('.codeicon').css({
-		position:'fixed',
+
 		left: ($(window).width() - $('.codeicon').outerWidth())/2,
 		top: ($(window).height() - $('.codeicon').outerHeight())/2
 	});
+
+
 
 	$('.buttons').css({
 		prosition:'fixed',
