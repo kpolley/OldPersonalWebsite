b533988cd0b8c7f407dadc0a27f69800d1847466
Kyle Polley <kpolley@outlook.com>
2016-12-18 21:34:42 -0800

    changed centering javascript, added knowledge logos, and changed experience transitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,25 @@
-$(window).resize(function(){
+$(function() {
+    $('.w3-card-4').css({
+        'position' : 'fixed',
+        'left' : '50%',
+        'top' : '50%',
+        'margin-left' : function() {return -$(this).outerWidth()/2},
+        'margin-top' : function() {return -$(this).outerHeight()/2}
+    });
 
-	$('#summary').css({
-
-		left: ($(window).width() - $('#summary').outerWidth())/2,
-		top: ($(window).height() - $('#summary').outerHeight())/2
-	});
-
-	$('#education').css({
-
-		left: ($(window).width() - $('#education').outerWidth())/2,
-		top: ($(window).height() - $('#education').outerHeight())/2
-	});
-
-	$('#experience').css({
-
-		left: ($(window).width() - $('#experience').outerWidth())/2,
-		top: ($(window).height() - $('#experience').outerHeight())/2
-	});
-
-	$('.experiences').css({
-
-		left: ($(window).width() - $('.experiences').outerWidth())/2,
-		top: ($(window).height() - $('.experiences').outerHeight())/2
-	});
-
-	$('#experience2').css({
-
-		left: ($(window).width() - $('#experience2').outerWidth())/2,
-		top: ($(window).height() - $('#experience2').outerHeight())/2
-	});
-
-	$('#projects').css({
-
-		left: ($(window).width() - $('#projects').outerWidth())/2,
-		top: ($(window).height() - $('#projects').outerHeight())/2
-	});
-
-	$('#knowledge').css({
-
-		left: ($(window).width() - $('#knowledge').outerWidth())/2,
-		top: ($(window).height() - $('#knowledge').outerHeight())/2
-	});
-
-	$('.codeicon').css({
-
-		left: ($(window).width() - $('.codeicon').outerWidth())/2,
-		top: ($(window).height() - $('.codeicon').outerHeight())/2
-	});
-
-
-
-	$('.buttons').css({
-		prosition:'fixed',
-		left: ($(window).width() - $('.buttons').width())/2,
-	});
-
+		$('.buttons').css({
+        'position' : 'fixed',
+        'left' : '50%',
+        'margin-left' : function() {return -$(this).outerWidth()/2}
+    });
 });
 
-// To initially run the function:
-$(window).resize();
-
 $(document).ready(function(){
-  $('.w3-panel').hide();
+
+
+  $('.w3-card-4').hide();
   setTimeout(function() {
-    $(".w3-panel").fadeIn(1000);
+    $(".w3-card-4").fadeIn(1000);
 }, 500);
 });
 
